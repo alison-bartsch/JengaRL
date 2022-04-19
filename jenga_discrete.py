0ce85c33a9503fa110ffd63cb9956a4974395975
Alison Bartsch <alisonsb@stanford.edu>
2022-04-19 16:18:32 -0400

    some edits for debugging

--- jenga_discrete.py
+++ jenga_discrete.py
@@ -14,8 +14,8 @@ class JengaEnv(gym.Env):
 		self.action_space = gym.spaces.Discrete(54)
 
 		# Define the state - cannot randomly initialize, because Jenga blocks are ordered
-		self.state=np.array(range(54)) 
-		print("State: ", self.state)
+		self.state = np.ones(3)	# 54
+
 		physicsClient = pb.connect(pb.DIRECT)
 
 		self.rendered_img = None
@@ -25,13 +25,14 @@ class JengaEnv(gym.Env):
 		planeId = pb.loadURDF('plane.urdf')
 		pb.setGravity(0, 0, -10, physicsClientId=physicsClient)
 		jengaId = pb.loadURDF('jenga/jenga.urdf', basePosition=[0,-0.05,0+.025*(1)])
+		print("Jenga ID: ", jengaId)
 		block_measure = tuple(map(lambda i, j: i - j, pb.getAABB(jengaId)[1], pb.getAABB(jengaId)[0]))
 		print("Block Measure: ", block_measure)
 
 		#block_height = 
 		#block_length = 
 		self.jengaObject=[]
-		for layer in range(18):
+		for layer in range(1):	# 18
 			if layer%2 ==1:
 				self.jengaObject.append(pb.loadURDF('jenga/jenga.urdf', basePosition=[0,-(0.05),0+.03*(layer+1)]))
 				self.jengaObject.append(pb.loadURDF('jenga/jenga.urdf', basePosition=[0,0,0+.03*(layer+1)]))
@@ -41,11 +42,12 @@ class JengaEnv(gym.Env):
 				self.jengaObject.append(pb.loadURDF('jenga/jenga.urdf', basePosition=[0,0,0+.03*(layer+1)], baseOrientation=[0,0,0.7071,0.7071]))
 				self.jengaObject.append(pb.loadURDF('jenga/jenga.urdf', basePosition=[(0.05),0,0+.03*(layer+1)], baseOrientation=[0,0,0.7071,0.7071]))
 
-		self.rewardBoard = pb.loadURDF("jenga/jenga.urdf", basePosition=[0,0,.03*(18)+0.05], globalScaling=3)
+		print("Jenga Object Length", len(self.jengaObject))
+		# self.rewardBoard = pb.loadURDF("jenga/jenga.urdf", basePosition=[0,0,.03*(18)+0.05], globalScaling=3)
 		pb.stepSimulation()
 		print("Created the Jenga Tower!")
 
-	def render(self, mode='human'):
+	def render(self, mode='human', pause=0.00001):
 		if self.rendered_img is None:
 			self.rendered_img = plt.imshow(np.zeros((100, 100, 4)))
 
@@ -62,30 +64,36 @@ class JengaEnv(gym.Env):
 		frame = np.reshape(frame, (100, 100, 4))
 		self.rendered_img.set_data(frame)
 		plt.draw()
-		plt.pause(3)
-		# plt.pause(.00001)
+		plt.pause(pause)
 
 	def step(self, sampleID):
 		pb.removeBody(self.jengaObject[sampleID]) #delete selected block
 
-		self.state = self.state[self.state != sampleID] #update state to describe remaining blocks
-		# print("State Shape: ", self.state.shape)
+		print(self.state.shape)
 
-		num_blocks = self.state.shape[0]
+		self.state[sampleID] = 0
+
+		print("state: ", self.state)
+
+		# num_blocks = self.state.shape[0]
+		num_blocks = np.sum(self.state)
 		for _ in range(100): 
 			pb.stepSimulation()
+			self.render()
+
 
 		reward = 54 - num_blocks #increase reward for more blocks removed from tower
 
-		print(pb.getBasePositionAndOrientation(self.rewardBoard)[0][2])
-		if pb.getBasePositionAndOrientation(self.rewardBoard)[0][2] > (0.03*17): #check if tower has fallen (if test board is still on top of the tower)
-			done = 0
-			print("Didn't Fall!")
-		else:
-			done = 1
-			reward = -1000
-			print("Tower Fell :(")
-		outputs = [self.state, reward, done]
+		# print(pb.getBasePositionAndOrientation(self.rewardBoard)[0][2])
+		# if pb.getBasePositionAndOrientation(self.rewardBoard)[0][2] > (0.03*17): #check if tower has fallen (if test board is still on top of the tower)
+		# 	done = 0
+		# 	print("Didn't Fall!")
+		# else:
+		# 	done = 1
+		# 	reward = -1000
+		# 	print("Tower Fell :(")
+		# outputs = [self.state, reward, done]
+		outputs = self.state, reward
 		return outputs
 
 	def reset(self):
@@ -95,15 +103,15 @@ class JengaEnv(gym.Env):
 
 # test code - see what is going on
 env = JengaEnv()
-env.render()
+env.render(pause=5)
 
 env.reset()
 
-for i in range(10):
+for i in range(3):
 	# remove the i+2 block
 	print("Removing block...")
-	state, reward, done = env.step(i+2)
-	env.render()
+	state, reward = env.step(2-i)
+	env.render(pause=3)
 
-	if done == 1:
-		assert False
+	# if done == 1:
+	# 	assert False
